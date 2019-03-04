531a8bbe7964620917f85bc5b15f300e512b9524
michaeldouglasLK <44294286+michaeldouglasLK@users.noreply.github.com>
2019-03-04 14:40:47 -0300

    Add empty project
    Add:
    - Move spacecraft left and right
    - Particle spacecraft effect
    - Background

---
+++ SpaceShooter/Assets/SkipControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, Input.GetAxis("Horizontal") * -30);
+        transform.Translate(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"), 0, Space.World);
+    }
+}
